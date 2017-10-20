e5cd9685ede27c2bcb97d2ac13c5f6a9ecc79806
Michael Bykov <m.bykov@gmail.com>
2017-10-20 15:35:30 +0300

    new sp-band

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import axios from 'axios'
 import App from './App'
 
-import {log, q, span, br, empty} from './utils'
-import band from '../../../speckled-band'
+import {log, q, span, empty, create} from './utils'
+import {zh} from '../../../speckled-band'
 const clipboard = require('electron-clipboard-extended')
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
@@ -15,30 +15,42 @@ clipboard
   .on('text-changed', () => {
     let currentText = clipboard.readText()
     console.log('TEXT:', currentText)
-    let code = 'zh'
-    let clauses = band(code, currentText)
-    let zh = clauses.map(cl => { return cl.cl }).join('')
-    if (!zh.length) return
-    // log('CL', clauses)
+    let pars = zh(currentText)
     let text = q('#text')
     empty(text)
-    clauses.forEach((clause) => {
-      let el
-      if (clause.cl) {
-        el = span(clause.cl)
-        el.classList.add('clause')
-        text.appendChild(el)
-      } else {
-        let strs = clause.sp.split('\n\n')
-        log('STRS', strs)
-        strs.forEach((str, idx) => {
-          el = span(str)
-          text.appendChild(el)
-          if (idx !== strs.length - 1) text.appendChild(br())
-        })
-        el.classList.add('space')
-      }
+    pars.forEach((cls) => {
+      let par = create('p')
+      cls.forEach((cl) => {
+        let spn = span(cl.text)
+        spn.classList = (cl.type === 'cl') ? 'cl' : 'sp'
+        par.appendChild(spn)
+      })
+      text.appendChild(par)
     })
+
+    // let code = 'zh'
+    // let clauses = band(code, currentText)
+    // let zh = clauses.map(cl => { return cl.cl }).join('')
+    // if (!zh.length) return
+    // // log('CL', clauses)
+    // clauses.forEach((clause) => {
+    //   let el
+    //   if (clause.cl) {
+    //     el = span(clause.cl)
+    //     el.classList.add('clause')
+    //     text.appendChild(el)
+    //   } else {
+    //     let strs = clause.sp.split('\n\n')
+    //     log('STRS', strs)
+    //     strs.forEach((str, idx) => {
+    //       el = span(str)
+    //       text.appendChild(el)
+    //       if (idx !== strs.length - 1) text.appendChild(br())
+    //     })
+    //     el.classList.add('space')
+    //   }
+    // })
+    log('PARS:', pars)
 
     // router.push({ path: '/main',  query: { clauses: clauses } })
   })
