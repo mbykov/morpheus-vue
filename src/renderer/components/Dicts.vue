98c4ffead46d2d3813d2b2369ddcec5f430de54d
Michael Bykov <m.bykov@gmail.com>
2017-10-20 23:24:35 +0300

    i

--- src/renderer/components/Dicts.vue
+++ src/renderer/components/Dicts.vue
@@ -5,16 +5,19 @@
       <div v-for="dn in dns">
         <br>
         <span class="dname">{{dn.dname}}</span>
-        <div v-for="doc in dn.docs">
-          <span class="pinyin">{{doc.pinyin}}</span>
+        <div v-for="(doc, idx) in dn.docs">
+          <span class="pinyin">{{idx + 1}}. {{doc.pinyin}}</span>
           <span class="pos">{{doc.pos}}</span>
           <!-- <span class="trns">{{doc.trns}}</span> -->
-          <div class="trns" v-for="(trn, idx) in doc.trns">
-            {{idx + 1}}. {{trn}}
+          <div class="trns" v-for="(trn, idy) in doc.trns">
+            {{trn}}
           </div>
+          <div class="note" v-if="doc.cf">note: {{doc.cf}}</div>
         </div>
       </div>
     </div>
+
+<!-- 他家白天一般 没有人，你还是晚上去吧。Tā jiā báitiā   !!!!!!!!!!!!!!! ОШИБКА !!             -->
 
     <!-- <hr> -->
     <!-- <p>{{dict}}</p> -->
