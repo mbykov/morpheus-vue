d80af917495b7ebbf96c783a7cfd6403c2fab3f9
Michael Bykov <m.bykov@gmail.com>
2017-11-23 01:24:38 +0300

    0.6.7961

--- src/renderer/sections/Title.vue
+++ src/renderer/sections/Title.vue
@@ -20,24 +20,24 @@
 // import {log} from '../utils'
 // const morpheuspng = 'static/256x256.png'
 
-export default {
-  name: 'title',
-  data: function () {
-    return {
-      // msrc: morpheuspng,
-      // mess: null
-    }
-  },
-  created () {
-    // log('TITLE CREATED')
-    // let that = this
-    // ipcRenderer.on('status', function (event, message) {
-    //   log('M', message)
-    //   that.mess = message
-    // })
-    // router.push({path: 'title', query: {title: 'title'}})
-  }
-}
+// export default {
+//   name: 'title',
+//   data: function () {
+//     return {
+//       // msrc: morpheuspng,
+//       // mess: null
+//     }
+//   },
+//   created () {
+//     // log('TITLE CREATED')
+//     // let that = this
+//     // ipcRenderer.on('status', function (event, message) {
+//     //   log('M', message)
+//     //   that.mess = message
+//     // })
+//     // router.push({path: 'title', query: {title: 'title'}})
+//   }
+// }
 </script>
 
 <style>
