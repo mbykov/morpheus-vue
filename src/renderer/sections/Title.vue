e86bfab9ab0e6fe0ea0694283083ff6ad5763974
Michael Bykov <m.bykov@gmail.com>
2017-11-23 00:36:47 +0300

    0.6.795

--- src/renderer/sections/Title.vue
+++ src/renderer/sections/Title.vue
@@ -1,22 +1,43 @@
 <template>
   <div id="title" class="section">
     ==========HHH
-    <!-- <div id="img"> -->
-    <!--   <\!-- <img :src="msrc"> -\-> -->
-    <!-- </div> -->
-    <!-- <div id="text"> -->
-    <!--   <p> -->
-    <!--     Copy some Chinese text from anywhere on your desktop (Ctrl-C); then move the mouse over text in order to see the readings of Chinese characters and obtain further information from various dictionaries.  </p> -->
-    <!--   <p>Try, for example, this phrase: 欢迎大家使用这个创新的软件，以便在瞬间学习新的汉字，并显着加快您的中文阅读速度！</p> -->
-    <!--   <p>Or the same in traditional characters: 歡迎大家使用這個創新的軟件，以便在瞬間學習新的漢字，並顯著加快您的中文閱讀速度！</p> -->
-    <!--   <\!-- <p>&nbsp;</p> -\-> -->
-    <!--   <\!-- <p>MESS: {{mess}}</p> -\-> -->
-    <!-- </div> -->
+    <div id="img">
+      <!-- <img :src="msrc"> -->
+    </div>
+    <div id="text">
+      <p>
+        Copy some Chinese text from anywhere on your desktop (Ctrl-C); then move the mouse over text in order to see the readings of Chinese characters and obtain further information from various dictionaries.  </p>
+      <p>Try, for example, this phrase: 欢迎大家使用这个创新的软件，以便在瞬间学习新的汉字，并显着加快您的中文阅读速度！</p>
+      <p>Or the same in traditional characters: 歡迎大家使用這個創新的軟件，以便在瞬間學習新的漢字，並顯著加快您的中文閱讀速度！</p>
+      <!-- <p>&nbsp;</p> -->
+      <!-- <p>MESS: {{mess}}</p> -->
+    </div>
   </div>
 </template>
 
-<script src='./title'>
-<!-- <script> -->
+<!-- <script src='./title'> -->
+<script>
+import {log} from '../utils'
+// const morpheuspng = 'static/256x256.png'
+
+export default {
+  name: 'title',
+  data: function () {
+    return {
+      // msrc: morpheuspng,
+      // mess: null
+    }
+  },
+  created () {
+    log('TITLE CREATED')
+    // let that = this
+    // ipcRenderer.on('status', function (event, message) {
+    //   log('M', message)
+    //   that.mess = message
+    // })
+    // router.push({path: 'title', query: {title: 'title'}})
+  }
+}
 </script>
 
 <style>
