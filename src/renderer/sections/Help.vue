f24547e92294d7ac7409a34c920c187b3d0c557c
Michael Bykov <m.bykov@gmail.com>
2017-11-23 18:49:21 +0300

    0.6.8

--- src/renderer/sections/Help.vue
+++ src/renderer/sections/Help.vue
@@ -4,7 +4,7 @@
     <p>just copy Crtl+C Chinese text everywhere on a desktop</p>
     <p><b>Shift</b> disables showing dictionaries (to move mouse freely)</p>
     <p><b>ESC</b> close all popups
-    <!-- <p><b>shift + mouse-wheel</b> scrolls other pane (to scroll dicts pane without moving mouse to it)</p> -->
+    <p><b>shift + mouse-wheel</b> scrolls other pane (to scroll dicts pane without moving mouse to it)</p>
     <p><b>click on a single symbol</b> shows dictionary and graphical data (thanks to MakeMeAHanzi project)
     <p><img id="mmhpng" :src="mmh"></p>
     <p><b>recursive segmentation</b>: long (more then one symbol) segment can be segmented further with mouse-click</p>
